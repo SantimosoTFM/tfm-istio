7c80c619f1ec3f8cb85781bb4bba95f4f4820658
Santiago Mora Soler <32168315+Santimoso@users.noreply.github.com>
2019-03-01 20:20:34 +0100

    Update inject_sidecar.sh

--- test/inject_sidecar.sh
+++ test/inject_sidecar.sh
@@ -1,1 +1,9 @@
-istioctl kube-inject -f network-super-tools-deploy.yaml -o k8s/network-super-tools-deploy-injected.yaml 
+kubectl -n istio-system get configmap istio-sidecar-injector -o=jsonpath='{.data.config}' > inject-config.yaml
+kubectl -n istio-system get configmap istio -o=jsonpath='{.data.mesh}' > mesh-config.yaml
+istioctl kube-inject \
+    --injectConfigFile inject-config.yaml \
+    --meshConfigFile mesh-config.yaml \
+    --filename network-super-tools-deploy.yaml \
+    --output k8s/network-super-tools-deploy-injected.yaml 
+kubectl apply -f k8s/
+#istioctl kube-inject -f network-super-tools-deploy.yaml -o k8s/network-super-tools-deploy-injected.yaml 
